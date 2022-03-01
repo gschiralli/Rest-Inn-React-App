2a78e03107425e494167825435d7536cc92ae0ca
Gianni Schiralli <gschiralli@myseneca.ca>
2022-03-01 17:30:19 -0500

    Hero section completed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,42 @@
+import img1 from "../assets/images/img1.jpg";
+import img2 from "../assets/images/img2.jpg";
+import img3 from "../assets/images/img3.jpg";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay, A11y } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <Swiper
+      // install Swiper modules
+      className="h-[80vh]"
+      modules={[Navigation, Pagination, A11y, Autoplay]}
+      slidesPerView={1}
+      navigation
+      autoplay={true}
+      pagination={{ clickable: true }}
+    >
+      <SwiperSlide>
+        <img
+          className="relative object-cover h-full w-full"
+          src={img1}
+          alt=""
+        />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className=" object-cover h-full w-full" src={img2} alt="" />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        {" "}
+        <img className=" object-cover h-full w-full" src={img3} alt="" />{" "}
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default Hero;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,45 @@
+import { useEffect, useState } from "react";
+
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
 function Homepage() {
+  const [properties, setProperties] = useState([
+    {
+      _id: 0,
+      title: "",
+      rentalPrice: 0,
+      description: null,
+      type: "",
+      houseRules: [],
+      amenities: [],
+      location: "",
+      bestSeller: null,
+      photoUrl: "",
+    },
+  ]);
+
+  useEffect(() => {
+    const URL = "https://intense-spire-46577.herokuapp.com/properties";
+    fetch(URL)
+      .then((response) => response.json())
+
+      .then((json) => {
+        setProperties(json.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <Header />
-      <main>
-        <Hero />
+      <Hero />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Property Types</h2>
+        </section>
       </main>
+      {console.log(properties)}
       <Footer />
     </div>
   );
