da04b03284918b415a69afb29933b24f6ba11c56
Gianni Schiralli <gschiralli@myseneca.ca>
2022-03-02 13:06:39 -0500

    homepage fix

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,28 +6,27 @@ import Hero from "../components/Hero";
 import TypeCard from "../components/TypeCard";
 
 function Homepage() {
-  const [properties, setProperties] = useState([
-    {
-      _id: 0,
-      title: "",
-      rentalPrice: 0,
-      description: null,
-      type: "",
-      houseRules: [],
-      amenities: [],
-      location: "",
-      bestSeller: null,
-      photoUrl: "",
-    },
-  ]);
+  const [propertyTypes, setPropertyTypes] = useState([]);
+  const [bestSellers, setBestSellers] = useState({
+    _id: 0,
+    title: "",
+    rentalPrice: 0,
+    description: null,
+    type: "",
+    houseRules: [],
+    amenities: [],
+    location: "",
+    bestSeller: null,
+    photoUrl: "",
+  });
 
   useEffect(() => {
-    const URL = "https://intense-spire-46577.herokuapp.com/properties";
+    const URL = "https://intense-spire-46577.herokuapp.com/properties/types";
     fetch(URL)
       .then((response) => response.json())
 
       .then((json) => {
-        setProperties(json.data);
+        setPropertyTypes(json.data);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -38,8 +37,9 @@ function Homepage() {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Property Types</h2>
-          {properties.map((item) => (
-            <TypeCard />
+          {console.log(propertyTypes)}
+          {propertyTypes.map((item, idx) => (
+            <TypeCard key={item + idx} />
           ))}
         </section>
       </main>
